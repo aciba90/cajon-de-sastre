2b40117d8826eee30f66d4ad3a3c4d51adce0592
Alberto Contreras <aciba90@gmail.com>
2023-12-25 22:06:23 +0100

    2022: solve day06 part2

--- src/bin/y22-d06.rs
+++ src/bin/y22-d06.rs
@@ -4,19 +4,19 @@ use aoc::include_data;
 
 const DATA: &str = include_data!("2022", "06");
 
-fn detect_start_of_packet_marker(msg: &str) -> Option<usize> {
-    let mut start = 4;
+fn detect_start_of_packet_with_distinct_chars(msg: &str, distinct_chars: usize) -> Option<usize> {
+    let mut start = distinct_chars;
 
     let chars: Vec<_> = msg.chars().collect();
-    for batch in chars.windows(4) {
-        let mut set = HashSet::with_capacity(4);
+    for batch in chars.windows(distinct_chars) {
+        let mut set = HashSet::with_capacity(distinct_chars);
         for x in batch {
             if !set.insert(x) {
                 // repeated element
                 break;
             }
         }
-        if set.len() == 4 {
+        if set.len() == distinct_chars {
             return Some(start);
         }
         start += 1;
@@ -25,12 +25,20 @@ fn detect_start_of_packet_marker(msg: &str) -> Option<usize> {
     None
 }
 
+fn detect_start_of_packet_marker(msg: &str) -> Option<usize> {
+    detect_start_of_packet_with_distinct_chars(&msg, 4)
+}
+
+fn detect_start_of_message_marker(msg: &str) -> Option<usize> {
+    detect_start_of_packet_with_distinct_chars(&msg, 14)
+}
+
 fn part1(data: &str) -> usize {
     detect_start_of_packet_marker(data).expect("Result not found")
 }
 
 fn part2(data: &str) -> usize {
-    todo!()
+    detect_start_of_message_marker(data).expect("Result not found")
 }
 
 fn main() {
